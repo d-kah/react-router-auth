125bf2eaf0de59f1ddb4ca1a63aca4f3ff96716f
macdavie911 <davidkahlenborn@gmail.com>
2020-03-11 22:14:16 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
+import { BrowserRouter as Router } from "react-router-dom";
 import './App.css';
 
-function App() {
+import AppRouter from "./Component/router";
+import Navbar from "./Component/Navbar";
+
+
+const App = () => {
+
   return (
     <div className="App">
-      <header className="App-header">
+
+      <Router>
+        <Navbar />
+        <AppRouter />
+      </Router>
+
+      {
+        /*
+          <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -18,7 +32,10 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header>  
+        */
+      }
+
     </div>
   );
 }
